5004e9adca26e88cd6521e7b7f6188604fac4716
agudamonte <agu.damonte@gmail.com>
2023-05-06 10:18:16 -0300

    added Modelo primer parcial solved

---
+++ modeloParcial/modeloParcial.cpp
@@ -0,0 +1,119 @@
+/* LaraKart es un torneo que se realiza todos los años en la Ciudad de Buenos Aires. Durante el lapso de un día los participantes pueden dirigirse a la pista de karting y realizar el circuito. Luego de que haya terminado el torneo se anunciará al ganador de la competencia.
+
+El organizador del torneo prevé que la edición 2023 tendrá una gran cantidad de participantes. 
+
+Cada participante realizará exactamente 4 vueltas a la pista de carrera
+
+Por cada vuelta de cada participante se registra:
+-	Código de participante (entero)
+-	Número de vuelta (1 a 4)
+-	Tiempo empleado (en minutos)
+-	Descalificado (1 - Sí; 0 - No)
+
+La información se encuentra agrupada por código de participante y por cada participante la información de las vueltas se encuentra ordenada por número de vuelta (de menor a mayor).
+!Para indicar el fin de la carga de datos se ingresa un código de participante negativo.
+
+Se pide calcular e informar
+A)	Informar el ganador de la competencia. El mismo es aquella persona que haya demorado menos tiempo en completar el circuito completo (las 4 vueltas) y nunca haya sido descalificado. Informar el código de participante y el tiempo demorado.
+
+NOTA: No habrá más de una persona que haya completado el circuito con el menor tiempo (ambiente ideal).
+  
+B)	La cantidad total de participantes que hayan sido descalificados al menos una vez.
+  
+C)	La cantidad total de participantes que no hayan sido descalificados en la última vuelta.
+  
+D)	Por cada participante, el porcentaje de vueltas descalificadas y el porcentaje de vueltas no descalificadas.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () { 
+  int codigoParticipante, numeroVuelta, qMinutos, i; 
+  bool descalificado;
+  const int CANTIDAD_VUELTAS = 4;
+
+  int acumuladorTiempoTotal;
+  bool puedeGanar = 1;
+  int ganador, tiempoMinimo = 0;
+  float descalificaciones, calificaciones;
+  int counterDescalificaciones = 0;
+  int counterCalificacionesUltimaVuelta = 0;
+
+  cout << "CODIGO PARTICIPANTE: ";
+  cin >> codigoParticipante;
+
+  while (codigoParticipante >= 0) {
+    
+    acumuladorTiempoTotal = 0;
+    descalificaciones = 0;
+    calificaciones = 0;
+    puedeGanar = 1;
+
+    for (i = 0; i < CANTIDAD_VUELTAS; i++){
+      
+      cout << "\tNUMERO DE VUELTA: ";
+      cin >> numeroVuelta;
+      
+      cout << "\tCANTIDAD DE MINUTOS: ";
+      cin >> qMinutos;
+      acumuladorTiempoTotal += qMinutos;
+
+      cout << "\tDESCALIFICADO? (1 - Sí; 0 - No): ";
+      cin >> descalificado;
+
+      if (descalificado == 1) {
+        descalificaciones++;
+        puedeGanar = 0;
+      } else {
+        calificaciones++;
+      }
+
+      if (i == 3 && descalificado ==0) {
+        counterCalificacionesUltimaVuelta++;
+      }
+
+    }
+
+    if (puedeGanar) {
+      if (tiempoMinimo == 0) {
+        tiempoMinimo = acumuladorTiempoTotal;
+        ganador = codigoParticipante;
+      } else {
+        if (acumuladorTiempoTotal <= tiempoMinimo) {
+          ganador = codigoParticipante;
+          tiempoMinimo = acumuladorTiempoTotal;
+        }
+      }
+    }
+
+    if (descalificaciones > 1) {
+      counterDescalificaciones++;
+    }
+    // D)
+    cout << "\n\nD) % de calificaciones y descalificaciones" << endl;
+    cout << "\t% CALIFICACIONES : " << float(calificaciones / 4 * 100) << " % " << endl;
+    cout << "\t% DESCALIFICACIONES : " << float(descalificaciones / 4 * 100 )<< " % " << endl << endl;
+
+    cout << "CODIGO PARTICIPANTE: ";
+    cin >> codigoParticipante;
+  }
+  
+
+  // A)
+  cout << "\nA) GANADOR" << endl;
+  cout << "\tEL GANADOR DE LA COMPETENCIA ES: " << ganador << endl;
+  cout << "\tEL TIEMPO DEL GANADOR FUE: " << tiempoMinimo << endl;
+
+  // B)
+  cout << "\nB)" << endl;
+  cout << "\tCANTIDAD DESCALIFICADOS MAS DE UNA VEZ: " << counterDescalificaciones << endl;
+
+  // C)
+  cout << "\nC)" << endl;
+  cout << "\tCANTIDAD CALIFICADOS EN LA ULTIMA VUELTA : " << counterCalificacionesUltimaVuelta<< endl;
+
+  
+  cout << endl << endl;
+  return 0;
+}
