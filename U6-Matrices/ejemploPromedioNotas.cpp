9986ab222a94b840cf18358b2a6bf25d8edd5da8
agudamonte <agu.damonte@gmail.com>
2023-05-27 10:11:05 -0300

    new activity added

---
+++ U6-Matrices/ejemploPromedioNotas.cpp
@@ -0,0 +1,17 @@
+/* Cargar las notas de los 78
+Crear un programa para ingresar las notas y devolver la cantidad de notas que
+estan por encima del promedio total
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int cantidadNotas;
+
+  cout << "Ingrese la cantidad de notas: ";
+
+  cin >> cantidadNotas;
+
+  return 0;
+}
---
+++ U6-Matrices/larapalooza.cpp
@@ -0,0 +1,37 @@
+/* Actividad Ejercicio examen final Larapalooza UTN
+
+El festival musical Larapalooza se realizó este año en el mes de Marzo y desea
+registrar la información de todos los artistas que participaron. Por cada
+artista y día registra:
+
+- Número de día (1 a 3)
+- Número de artista (100 a 130)
+- Género musical del artista (1 a 25)
+- Cantidad de minutos de show (entero)
+- Cantidad de canciones a tocar (entero)
+
+Aclaraciones:
+- Un mismo artista puede tocar en varios días.
+- Un artista pertenece a un género y sólo uno.
+
+El fin de los datos se indica con un número de día igual a 0. Los datos no están
+agrupados ni ordenados.
+
+Calcular e informar:
+El género musical que más minutos tocó el día 3.
+La cantidad de artistas que hayan participado en los tres días del recital.
+Los números de artistas que hayan tocado más de 15 temas en el recital (teniendo
+en cuenta todos los días en los que hayan participado). Los géneros que no hay
+participado en absoluto del recital.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int dia, artista, genero, minutos, canciones;
+
+  return 0;
+}
