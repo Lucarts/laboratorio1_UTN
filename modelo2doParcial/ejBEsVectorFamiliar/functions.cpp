e9fbadc4bfbe8effaf6643d1af2db07d9d9a2a8a
agudamonte <agu.damonte@gmail.com>
2023-06-21 09:10:51 -0300

    EsVectorFamiliar updated

--- modelo2doParcial/ejBEsVectorFamiliar/functions.cpp
+++ modelo2doParcial/ejBEsVectorFamiliar/functions.cpp
@@ -1,3 +1,15 @@
+/* Modelo de Segundo parcial Problema 1 - 25p
+
+Un vector de números enteros es Familiar si todos los elementos que lo componen son números primos. Hacer una función llamada EsVectorFamiliar que reciba dos parámetros: un vector de números enteros y un número entero que corresponde al tamaño del vector. 
+
+La función debe devolver true si el vector es Familiar o false en caso contrario.
+Aclaración: La función debe llamarse como se indica en el enunciado. 
+La función no debe solicitar ningún dato al usuario ni mostrar ninguna información por pantalla. 
+No deberá entregarse la función main para este problema. Pueden utilizarse funciones auxiliares si las necesitan.
+Definición de número primo: En matemáticas, un número primo es un número natural mayor que 1 que tiene únicamente dos divisores positivos distintos: él mismo y el 1.
+
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,8 +18,8 @@ using namespace std;
 bool EsVectorFamiliar(int array[], int arrayLength);
 
 int main() {
-  int longitudArray = 4;
-  int arrayPrimos[longitudArray] = {7, 3, 5, 4};
+  int longitudArray = 3;
+  int arrayPrimos[longitudArray] = {7, 3, 5};
 
   bool esArrayPrimo = EsVectorFamiliar(arrayPrimos, longitudArray);
 
@@ -30,11 +42,7 @@ bool EsVectorFamiliar(int array[], int arrayLength) {
       }
     }
 
-    if (counterDivisores == 2) {
-      esFamiliar = true;
-    } else {
-      esFamiliar = false;
-    }
+    esFamiliar = (counterDivisores == 2);
   }
 
   return (esFamiliar);
